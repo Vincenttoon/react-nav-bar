19efa247b06c2de40f8031cc4a93c375492d3744
Vincent Toon <vincenttoon22@gmail.com>
2024-01-09 16:09:12 -0600

    finished without Link from RRD

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
-import Header from './components/Header.js';
+import Layout from "./components/Layout.js";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import Home from "./pages/Home.js";
+import PageOne from "./pages/PageOne.js";
+import PageTwo from "./pages/PageTwo.js";
+import PageThree from "./pages/PageThree.js";
+import PageCTA from "./pages/PageCTA.js";
 
 function App() {
   return (
-    <>
-      <Header />
-    </>
-  )
+    <Layout>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/pageOne" element={<PageOne />} />
+          <Route path="/pageTwo" element={<PageTwo />} />
+          <Route path="/pageThree" element={<PageThree />} />
+          <Route path="/ctaPage" element={<PageCTA />} />
+        </Routes>
+      </Router>
+    </Layout>
+  );
 }
 
 export default App;
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
 
 import { BiMenuAltRight } from "react-icons/bi";
 import { AiOutlineClose } from "react-icons/ai";
@@ -6,49 +7,69 @@ import { AiOutlineClose } from "react-icons/ai";
 import classes from "./Header.module.scss";
 
 const Header = () => {
-    const [menuOpen, setMenuOpen] = useState(false);
-    const [size, setSize] = useState({
-        width: undefined,
-        height: undefined,
-    })
+  const [menuOpen, setMenuOpen] = useState(false);
+  const [size, setSize] = useState({
+    width: undefined,
+    height: undefined,
+  });
 
-    useEffect(() => {
-        const handleResize = () => {
-            setSize({
-                width: window.innerWidth,
-                height: window.innerHeight,
-            });
-        };
-        window.addEventListener("resize", handleResize);
+  useEffect(() => {
+    const handleResize = () => {
+      setSize({
+        width: window.innerWidth,
+        height: window.innerHeight,
+      });
+    };
+    window.addEventListener("resize", handleResize);
 
-        return () => window.removeEventListener("resize", handleResize);
-    })
+    return () => window.removeEventListener("resize", handleResize);
+  });
 
-    const menuToggleHandler = () => {
-        setMenuOpen((p) => !p);
-    }
+  const menuToggleHandler = () => {
+    setMenuOpen((p) => !p);
+  };
+
+  const ctaClickHandler = () => {
+    
+  }
 
   return (
     <header className={classes.header}>
       <div className={classes.header__content}>
         <h2 className={classes.header__content__logo}>navbar</h2>
 
-        <nav className={`${classes.header__content__nav} ${menuOpen ? classes.isMenu : ""}`}>
+        <nav
+          className={`${classes.header__content__nav} ${
+            menuOpen && size.width < 768 ? classes.isMenu : ""
+          }`}
+        >
           <ul>
             <li>
-              <a href="/">PageOne</a>
+              <a href="/pageOne" onClick={menuToggleHandler}>
+                PageOne
+              </a>
             </li>
             <li>
-              <a href="/">PageTwo</a>
+              <a href="/pageTwo" onClick={menuToggleHandler}>
+                PageTwo
+              </a>
             </li>
             <li>
-              <a href="/">PageThree</a>
+              <a href="/pageThree" onClick={menuToggleHandler}>
+                PageThree
+              </a>
             </li>
           </ul>
-          <button>CTA Page</button>
+          <button onClick={menuToggleHandler}>
+            <a href="/ctaPage">CTA Page</a>
+          </button>
         </nav>
         <div className={classes.header__content__toggle}>
-          {menuOpen ? <AiOutlineClose onClick={menuToggleHandler} />  :<BiMenuAltRight onClick={menuToggleHandler} />}
+          {menuOpen ? (
+            <AiOutlineClose onClick={menuToggleHandler} />
+          ) : (
+            <BiMenuAltRight onClick={menuToggleHandler} />
+          )}
         </div>
       </div>
     </header>
