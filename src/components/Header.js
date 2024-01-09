b0f2072d7a959ceb763c50a158716dd25e06ae35
Vincent Toon <vincenttoon22@gmail.com>
2024-01-09 15:19:19 -0600

    header functional

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 import { BiMenuAltRight } from "react-icons/bi";
 import { AiOutlineClose } from "react-icons/ai";
@@ -6,11 +6,34 @@ import { AiOutlineClose } from "react-icons/ai";
 import classes from "./Header.module.scss";
 
 const Header = () => {
+    const [menuOpen, setMenuOpen] = useState(false);
+    const [size, setSize] = useState({
+        width: undefined,
+        height: undefined,
+    })
+
+    useEffect(() => {
+        const handleResize = () => {
+            setSize({
+                width: window.innerWidth,
+                height: window.innerHeight,
+            });
+        };
+        window.addEventListener("resize", handleResize);
+
+        return () => window.removeEventListener("resize", handleResize);
+    })
+
+    const menuToggleHandler = () => {
+        setMenuOpen((p) => !p);
+    }
+
   return (
     <header className={classes.header}>
       <div className={classes.header__content}>
         <h2 className={classes.header__content__logo}>navbar</h2>
-        <nav className={classes.header__content__nav}>
+
+        <nav className={`${classes.header__content__nav} ${menuOpen ? classes.isMenu : ""}`}>
           <ul>
             <li>
               <a href="/">PageOne</a>
@@ -25,7 +48,7 @@ const Header = () => {
           <button>CTA Page</button>
         </nav>
         <div className={classes.header__content__toggle}>
-          <BiMenuAltRight />
+          {menuOpen ? <AiOutlineClose onClick={menuToggleHandler} />  :<BiMenuAltRight onClick={menuToggleHandler} />}
         </div>
       </div>
     </header>
