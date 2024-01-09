3cdcd85ea7c8fbb0d2ac29ae9c1cc80e2f4a2c9f
Vincent Toon <vincenttoon22@gmail.com>
2024-01-09 14:59:37 -0600

    scss for nav set up

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,36 +1,35 @@
-import React from 'react'
+import React from "react";
 
-import { BiMenuAltRight } from 'react-icons/bi';
-import { AiOutlineClose } from 'react-icons/ai'
+import { BiMenuAltRight } from "react-icons/bi";
+import { AiOutlineClose } from "react-icons/ai";
 
-
-import classes from './Header.module.scss';
+import classes from "./Header.module.scss";
 
 const Header = () => {
   return (
     <header className={classes.header}>
-        <div className={classes.header__content}>
-            <h2>navbar</h2>
-        </div>
+      <div className={classes.header__content}>
+        <h2 className={classes.header__content__logo}>navbar</h2>
         <nav className={classes.header__content__nav}>
-            <ul>
-                <li>
-                    <a href='/'>PageOne</a>
-                </li>
-                <li>
-                    <a href='/'>PageTwo</a>
-                </li>
-                <li>
-                    <a href='/'>PageThree</a>
-                </li>
-            </ul>
-            <button>CTA Page</button>
+          <ul>
+            <li>
+              <a href="/">PageOne</a>
+            </li>
+            <li>
+              <a href="/">PageTwo</a>
+            </li>
+            <li>
+              <a href="/">PageThree</a>
+            </li>
+          </ul>
+          <button>CTA Page</button>
         </nav>
         <div className={classes.header__content__toggle}>
-            <BiMenuAltRight />
+          <BiMenuAltRight />
         </div>
+      </div>
     </header>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
