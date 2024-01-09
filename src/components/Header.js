5da9a2a9c4bd0318845f437abac4e0f3c94553fb
Vincent Toon <vincenttoon22@gmail.com>
2024-01-09 14:34:13 -0600

    initial scss and file setup created

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import classes from './Header.module.scss';
+
+const Header = () => {
+  return (
+    <header className={classes.header}>
+      
+    </header>
+  )
+}
+
+export default Header
